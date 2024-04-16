03fdfaa61915ffd4656f14ffefd63ec1feda533d
Devika KB <kbdevika20@gmail.com>
2024-04-16 14:29:03 +0530

    log

--- placementapp-main/controllers/login.js
+++ placementapp-main/controllers/login.js
@@ -4,7 +4,7 @@ import { pool } from "./../config.js";
 
 router.post("/", async (req, res) => {
     try {
-      const { Password_Hash } = req.body;
+      const { Password_Hash,Role,Email } = req.body;
   
       if (!Password_Hash) {
         return res.status(400).json({ message: "Password_Hash is required" });
@@ -12,8 +12,16 @@ router.post("/", async (req, res) => {
   
       const client = await pool.connect();
       const query =
-        "SELECT Role FROM users WHERE Password_Hash = $1"; // Selecting Role based on Password_Hash
-      const values = [Password_Hash];
+        "SELECT Role,Password_Hash FROM users WHERE Email = $1"; 
+      const values = [Password_Hash,Role];
+      if(result.rows[0].Password_Hash.toLowercase() === Password_Hash.toLowercase()){
+        res.status(500).json({ message: "User authenticated" });
+      }
+      else{
+        res.status(500).json({ message: "password is incorrect" });
+      }
+  
+
   
       const result = await client.query(query, values);
       client.release();
